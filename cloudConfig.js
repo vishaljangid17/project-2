4ca26797d6d1ab7c4d403a85580c2db89734c509
Vishal Jangid <vishaljangid1712@gmail.com>
2026-09-18 16:03:54 +0530

    Add Project Files

--- cloudConfig.js
+++ cloudConfig.js
@@ -5,7 +5,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
     api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECETE 
+    api_secret:process.env.CLOUD_API_SECRET
 })
 
 const storage = new CloudinaryStorage({
